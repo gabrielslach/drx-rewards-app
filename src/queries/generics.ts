d9b3f5814db9e451fad8297e1a20a75c3a7ca8cb
gabrielslach <gabdl.dev@gmail.com>
2022-06-01 09:24:33 +0800

    :sparkles: Transactions special query

--- src/queries/generics.ts
+++ src/queries/generics.ts
@@ -2,5 +2,12 @@ export default {
     selectAll: (tableName: string) => `SELECT * FROM ${tableName}`,
     selectID: (tableName: string) => `SELECT * FROM ${tableName} WHERE id=$1`,
     insert: (tableName: string, fieldKeys: string[]) => `INSERT INTO ${tableName}(${fieldKeys.toString()}) VALUES(${fieldKeys.map((_, i) => `$${i + 1}`).toString()})`,
+    aggregatePoints: (type: string) => `
+        SELECT COALESCE(SUM(T.points)) AS total_points
+        FROM transactions t 
+        INNER JOIN categories cat ON t.category_id = cat.id
+        WHERE cat.type = '${type}' AND t.customer_id = $1
+        GROUP BY (cat.type);
+        `,
 
 }
--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,19 +1,89 @@
 import { FastifyPluginAsync } from 'fastify'
 import generics from '../queries/generics'
+import { PoolClient } from 'pg';
 
 const tableName = "transactions";
+
+
+const getTransactionsSchema = {
+  query: {
+    type: "object",
+    properties: {
+      source_id: {
+        type: "number",
+      },
+      category_name: {
+        type: "string",
+      },
+      type: {
+        type: "string",
+      },
+    },
+  },
+  response: {
+    success: {
+      type: "boolean"
+    }
+  }
+}
+
+type getTransactionQuery = {
+  source_id: number;
+  category_name: string;
+  type: string;
+}
+
+const fieldMapping: {[key: string]: any} = {
+  category_name: 'cat.name',
+  type: 'cat.type',
+  source_id: 's.id',
+}
+
+const transactionQueryConstructor = (query: getTransactionQuery) => {
+  const dbQuery = [
+    `
+      SELECT t.id transaction_id, cus.name customer_name, s.label source_label, cat.type category_type, cat.name category_name, T.points total_points
+      FROM transactions t 
+      INNER JOIN customers cus ON t.customer_id = cus.id
+      INNER JOIN sources s ON t.source_id = s.id
+      INNER JOIN categories cat ON t.category_id = cat.id
+    `,
+  ];
+
+  Object.entries(query).forEach((q, index) => {
+    const [key, val] = q;
+    if (index === 0) {
+      dbQuery.push('WHERE')
+    } else {
+      dbQuery.push('AND')
+    }
+
+    let _val;
+    if (typeof val === 'string') {
+      _val = `'${val}'`;
+    } else {
+      _val = val;
+    }
+
+    dbQuery.push(`${fieldMapping[key]} = ${_val}`)
+  });
+
+  return dbQuery.join(' ');
+}
+
 const transactions: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/transactions', async function (request, reply) {
+  fastify.get<{ Querystring: getTransactionQuery }>('/transactions',{ schema: getTransactionsSchema }, async function (request, reply) {
+    const query = request.query;
+    if ((query.category_name && !query.source_id) ||
+      (!query.category_name && query.source_id)) {
+      return { success: false, message: 'Invalid query.' }
+    }
     const client = await fastify.pg.connect();
     try {
+      const dbQuery = transactionQueryConstructor(query);
+      
       const { rows } = await client.query(
-        `
-          SELECT t.id transactionID, cus.name customerName, s.label sourceLabel, cat.type categoryType, cat.name categoryName, T.points totalPoints
-          FROM transactions t 
-          INNER JOIN customers cus ON t.customer_id = cus.id
-          INNER JOIN sources s ON t.source_id = s.id
-          INNER JOIN categories cat ON t.category_id = cat.id
-        `
+        dbQuery
       );
 
       return rows;
@@ -22,7 +92,7 @@ const transactions: FastifyPluginAsync = async (fastify, opts): Promise<void> =>
     }
   })
   
-  const transactionSchema = {
+  const postTransactionSchema = {
     body: {
       type: "object",
       properties: {
@@ -55,10 +125,39 @@ const transactions: FastifyPluginAsync = async (fastify, opts): Promise<void> =>
     points: number;
   }
 
-  fastify.post<{ Body: postTransactionBody }>('/transaction', { schema: transactionSchema }, async function (request, reply) {
+  const calculateBalance = async (client: PoolClient, customerID: number) => {
+    const {rows: earnedRowsSum} = await client.query(
+      generics.aggregatePoints('earned'),
+      [customerID]
+    );
+    
+    const {rows: spentRowsSum} = await client.query(
+      generics.aggregatePoints('spent'),
+      [customerID]
+    );
+
+    const totalEarned = earnedRowsSum[0] ? earnedRowsSum[0].total_points : 0;
+    const totalSpent = spentRowsSum[0] ? spentRowsSum[0].total_points : 0;
+    
+    return totalEarned - totalSpent;
+  }
+
+  fastify.post<{ Body: postTransactionBody }>('/transaction', { schema: postTransactionSchema }, async function (request, reply) {
     const client = await fastify.pg.connect();
     try {
       const body = request.body;
+
+      const { rows: categoryRow } = await client.query(
+        'SELECT type FROM categories WHERE id = $1',
+        [body.categoryID]
+      );
+
+      if (categoryRow[0] && categoryRow[0].type === 'spent') {
+        if (await calculateBalance(client, body.customerID) < body.points) {
+          return { success: false, message: "No enough balance." }
+        }
+      }
+
       await client.query(
         generics.insert(tableName, ['source_id', 'category_id', 'customer_id', 'points']),
         [body.sourceID, body.categoryID, body.customerID, body.points]
